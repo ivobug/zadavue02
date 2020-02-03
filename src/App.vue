79c8400b260f5255253b7d519dba92ca9d23892d
ivobug <ivo.bagaric.2@gmail.com>
2020-02-03 02:07:19 +0100

    last

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,13 @@
       <img v-bind:src="image">
     </div>
     <div class="textPlace">
+
       <span>{{joke}}</span>
     </div>
     <br>
-    <button @click="axiosFunk()">Get joke</button>
+    <button @click="getRandomCategory()">Get joke</button>
+    {{pickedCategory}}
+
 
   </div>
 </template>
@@ -15,8 +18,8 @@
 <script>
 import axios from 'axios';
 import axiosTiming from 'axios-timing'
-
-
+// Koristio sam dva nacina za mjernje vremena requesta (oba su u konzoli)
+// 1. Putem axios Timinga (koji je precizniji)
 axiosTiming(axios, window.console.log)
 
 export default {
@@ -27,27 +30,45 @@ export default {
       image:null,
       errors:[],
       start:null,
-      diff:null
+      diff:null,
+      categories:[],
+      pickedCategory:null,
     }
   },
   methods:{
     axiosFunk:function(){
-      // uzimamo trenutno vrijeme na pocetku axios funkcije te njime oduzimamo vrijeme
+      // 2. uzimamo trenutno vrijeme na pocetku axios funkcije te njime oduzimamo vrijeme
       // zavrsetka axios funkcije.. milisekunde ispisujemo u consolu
       this.start=new Date().getTime()
-      axios.get(`https://api.chucknorris.io/jokes/random`)
+      axios.get(`https://api.chucknorris.io/jokes/random?categories=${this.pickedCategory}`)
       .then(response => {
         this.diff=new Date().getTime()-this.start
         this.joke=response.data.value
         this.image= response.data.icon_url
-        window.console.log(this.diff)
+        window.console.log(`drugi rezultat ${this.diff} ms `)
+
       })
       .catch(e => {
         this.errors.push(e)
       })
     },
+    // Biramo random broj te kategoriju koja je pod tim rednim brojem spremamo u pickedCategory
+    // te pozivamo axiosFunk
+    getRandomCategory:function(){
+      let randomNum = Math.floor(Math.random() * this.categories.length)
+      this.pickedCategory= this.categories[randomNum]
+      this.axiosFunk()
 
-
+    }
+    },
+    // Pri pokretanju stranice dohvaćamo kategorije te ih spremamo u categories array
+    // i ispisujemo ih u konzoli
+  created(){
+    axios.get(`https://api.chucknorris.io/jokes/categories`)
+    .then(response => {
+    this.categories=response.data
+    window.console.log(this.categories)
+      })
     }    
   }
 
