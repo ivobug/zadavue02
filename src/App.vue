6039debf9495e5c4cf1f0af5f6492b36cf805d04
ivobug <ivo.bagaric.2@gmail.com>
2020-02-03 01:05:43 +0100

    prvi

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,89 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="imagePlace">
+      <img v-bind:src="image">
+    </div>
+    <div class="textPlace">
+      <span>{{joke}}</span>
+    </div>
+    <br>
+    <button @click="axiosFunk()">Get joke</button>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+import axiosTiming from 'axios-timing'
+
+
+axiosTiming(axios, window.console.log)
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      joke:null,
+      image:null,
+      errors:[],
+      start:null,
+      diff:null
+    }
+  },
+  methods:{
+    axiosFunk:function(){
+      // uzimamo trenutno vrijeme na pocetku axios funkcije te njime oduzimamo vrijeme
+      // zavrsetka axios funkcije.. milisekunde ispisujemo u consolu
+      this.start=new Date().getTime()
+      axios.get(`https://api.chucknorris.io/jokes/random`)
+      .then(response => {
+        this.diff=new Date().getTime()-this.start
+        this.joke=response.data.value
+        this.image= response.data.icon_url
+        window.console.log(this.diff)
+      })
+      .catch(e => {
+        this.errors.push(e)
+      })
+    },
+
+
+    }    
   }
-}
+
 </script>
 
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 10px;
+}
+div{
+  display: inline-block;
+  margin: 5px;
+}
+.imagePlace{
+  background-color: rgb(235, 172, 101);
+  border-radius: 10px;
+  width: 70px;
+  height: 70px;
+
+}
+.textPlace{
+    width: 700px;
+    height: 70px;
+    border-radius: 10px;
+    background-color: rgb(235, 235, 235);
+    position: absolute;
+    padding: 5px;
+
+}
+button{
+  margin: 10px;
+}
+
+img{
+    margin: 5px;
+
 }
 </style>
